286130ad0910b9d0deccbf11b1f7a3cd62af476e
vadmin <vadmin@ANX237.fsktm.edu>
2019-03-08 20:10:35 +0800

    add file

---
+++ Newton.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  8 19:21:07 2019
+
+@author: vadmin
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+#from numpy.linalg import inv
+from mpl_toolkits.mplot3d import Axes3D
+
+def func(x):
+    return 100*np.square(np.square(x[0])-x[1])+np.square(x[0]-1)
+
+def dfunc(x):
+    df1 = 400*x[0]*(np.square(x[0])-x[1])+2*(x[0]-1)
+    df2 = -200*(np.square(x[0])-x[1])
+    return np.array([df1,df2])
+
+def invhess(x):
+    df11 = 1200*np.square(x[0])-400*x[1]+2
+    df12 = -400*x[0]
+    df21 = -400*x[0]
+    df22 = 200
+    hess = np.array([[df11,df12],[df21,df22]])
+    return np.linalg.inv(hess)
+
+def grad(x, max_int):
+    miter = 1
+    step = .5
+    vals = []
+    objectfs = []
+    while miter <= max_int:
+        vals.append(x)
+        objectfs.append(func(x))
+        print(x,func(x),miter)
+#        temp = x-step*dfunc(x)
+        temp = x-step*(invhess(x).dot(dfunc(x)))
+        if np.abs(func(temp)-func(x))>0.01: #stop here if the comparison is too small this value is up to us
+            x = temp
+        else:
+            break
+        miter +=1
+    return vals, objectfs, miter
+
+
+
+start = [5,5]
+val, objectf, iters = grad(start,50)
+
+x = np.array([i[0] for i in val])
+y = np.array([i[1] for i in val])
+z = np.array(objectf)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.scatter(x,y,z, label='newton method')
+ax.legend()
+plt.savefig('newton.jpg')
---
+++ gradient_descent.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+author: aswadi
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def func(x):
+    return 100*np.square(np.square(x[0])-x[1])+np.square(x[0]-1) #calculate the z
+
+def dfunc(x):
+    df1 = 400*x[0]*(np.square(x[0])-x[1])+2*(x[0]-1)
+    df2 = -200*(np.square(x[0])-x[1])
+    return np.array([df1,df2])
+
+def grad(x, max_int):
+    miter = 1
+    step = .0001/miter
+    vals = []
+    objectfs = []
+    while miter <= max_int:
+        vals.append(x)
+        objectfs.append(func(x))
+        print(x,func(x),miter)
+        temp = x-step*dfunc(x)  # 
+        if np.abs(func(temp)-func(x))>0.01: #stop here if the comparison is too small this value is up to us
+            x = temp
+        else:
+            break        
+        miter +=1
+    return vals, objectfs, miter
+
+start = [5,5]
+val, objectf, iters = grad(start,50)
+
+x = np.array([i[0] for i in val])
+y = np.array([i[1] for i in val])
+z = np.array(objectf)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.scatter(x,y,z, label='gradient descent method')
+ax.legend()
+plt.savefig('GradientDescent.jpg')
