6262160cd70b9eda3ec20bf778007feb5eccfbd3
vadmin <vadmin@ANX237.fsktm.edu>
2019-03-29 19:27:22 +0800

    update

---
+++ exercise/exercise4/main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 29 15:03:59 2019
+
+@author: Ong Sim Ying
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from linalg_utils import *
+import trust_region as tr
+import step_finders as sf
+
+f1 = lambda x: 100 * (x[1] - x[0]**2)**2 + (1 - x[0])**2
+g1 = lambda x: np.array([-400*(x[1] - x[0]**2)*x[0] - 2*(1-x[0]), 200*(x[1] - x[0]**2)])
+h1 = lambda x: np.array([
+    [-400*(x[1] - 3*x[0]**2) + 2, -400*x[0]],
+    [-400*x[0], 200]]
+)
+x1s = [np.array([2, 1]), np.array([0, 1]), np.array([-1, 1])]
+
+a2 = lambda x: np.exp(-1. / (100*(x[0]-1))**2)
+f2 = lambda x: x[0]**2 + a2(x) - 1
+g2 = lambda x: np.array([2*x[0] + a2(x)*200*(1./(100*(x[0]-1))**3)])
+h2 = lambda x: np.array([2 + a2(x)*((200*(1./(100*(x[0]-1))**3))**2 + 600*(1./(100*(x[0]-1))**4))])
+x2s = [np.array([-1]), np.array([-2])]
+
+x, fx, iterations = tr.trust_region(f2, g2, h2, x2s[0], .1, 1, .15, sf.cauchy_point_step_finder)
+print('Result in %d iterations:' % iterations)
+print('%s -> %f' % (x, fx))
---
+++ exercise/exercise4/trust_region.py
@@ -0,0 +1,36 @@
+from linalg_utils import *
+
+
+def model(f, g, b, x, p, delta):
+    if la.norm(p) > delta:
+        raise ArithmeticError('P must not be bigger than delta, p and delta:', p, la.norm(p), delta)
+    return f(x) + np.matmul(g(x).T, p) + .5*np.matmul(np.atleast_1d(np.matmul(p.T, b)), p)
+
+def trust_region(f, g, hf, x0, delta_0, max_delta, etha, step_finder, repair_hessian=True, eps=1e-5):
+
+    x = x0
+    delta = delta_0
+    iterations = 0
+
+    while True:
+
+        iterations += 1
+        b = hf(x)
+        if repair_hessian:
+            b = repair_psd(b)
+
+        p = step_finder(g(x), b, delta)
+        rho = (f(x) - f(x+p)).astype('f') / (model(f, g, b, x, p, delta) - model(f, g, b, x+p, p, delta))
+
+        if rho < .25:
+            delta = .25 * delta
+        elif rho >= .75 and np.isclose(la.norm(p), delta, 1e-4):
+            delta = min(2*delta, max_delta)
+
+        if rho > etha:
+            x = x + p
+        elif np.allclose(p, np.zeros(p.shape), eps):
+            result = x + p
+            break
+
+    return result, f(result), iterations
